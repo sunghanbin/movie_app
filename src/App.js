e8dc2280c5baa01ab43deccb0b783d8f67af4017
sunghanbin <gksqlsekdna@gmail.com>
2020-05-25 14:51:15 +0900

    state

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import PropTypes from "prop-types";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// state 에 대하여...
+
+class App extends React.Component {
+  // 동적 데이터 state
+  state = {
+    count: 0,
+  };
+
+  // 이벤트 메소드 작성
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
+
+  // calss 는 return 이 아닌 render 를 가지고 있다.
+  // 클래스에서는 뷰를 뿌릴때  render 를 사용한다.
+  // this.state.count state 사용때는 this 붙혀준다.
+
+  render() {
+    return (
+      <div>
+        {" "}
+        <h1>Im a class {this.state.count}</h1>
+        <button onClick={this.add}>plus</button>
+        <button onClick={this.minus}>minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
